e6bfe6174da0da511291a4f8d7be9a3dd5eb1d85
Hrayr77 <hrayr7711@gmail.com>
2021-12-02 22:23:16 +0400

    feat:tasc is ready

---
+++ tascTriangle.js
@@ -0,0 +1,12 @@
+let angl1 = 47;
+let angl2 = 53;
+let angl3;
+let triangle = 180;
+
+
+if(angl1 + angl2 >= triangle){
+console.log("ankap");
+}else{
+    angl3 = triangle - angl1 - angl2;
+    console.log(angl3);
+}
