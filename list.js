fae8ccc12eae0c525628088c4090efc0ebe52b85
hakobyanhrayr <hrayr7711@gmail.com>
2021-12-04 23:25:29 +0400

    fix:tasc is ready

--- list.js
+++ list.js
@@ -2,18 +2,22 @@
 let mul = 1;
 let quot = 0;
 let rem = 0;
-let num = 5768;
+let num = 5678;
 let sum = 0;
 let count = 0;
-while (num > 10){
-    count = num % 10;
-    num = (num - count) / 10;
-    sum += count;
-    mul*=count;
-    if(num < 10){
-        sum +=num;
-        mul *=num;
+if(num !== 0){
+    while (num > 10){
+        count = num % 10;
+        num = (num - count) / 10;
+        sum += count;
+        mul*=count;
+        if(num < 10){
+            sum +=num;
+            mul *=num;
+        }
     }
+}else {
+    console.log("can note");
 }
 console.log(mul);
 console.log(sum);
