079b0af6480209b072b78364ad74ad0bd3258759
hakobyanhrayr <hrayr7711@gmail.com>
2021-12-04 22:56:10 +0400

    feat:tasc redy

--- list.js
+++ list.js
@@ -1,17 +1,29 @@
 "use strict"
+let mul = 1;
 let quot = 0;
 let rem = 0;
-let num = 63;
-let count = num % 10;
-num = (num -count) / 10;
-let sum = count + num;
-let nul = count * num;
+let num = 5768;
+let sum = 0;
+let count = 0;
+while (num > 10){
+    count = num % 10;
+    num = (num - count) / 10;
+    sum += count;
+    mul*=count;
+    if(num < 10){
+        sum +=num;
+        mul *=num;
+    }
+}
+console.log(mul);
+console.log(sum);
 
-if (nul % sum !== 0) {
-    quot = nul % sum;
+
+if (mul % sum !== 0) {
+    quot = mul % sum;
     console.log(quot);
 }
 else{
-    rem = nul / sum;
+    rem = mul / sum;
     console.log(rem);
 }
