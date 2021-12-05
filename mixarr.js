a6b5f7b739444ed6ed9a3b1c5e1d90989186f5ca
hakobyanhrayr <hrayr7711@gmail.com>
2021-12-05 21:51:57 +0400

    feat: tasc is ready

---
+++ mixarr.js
@@ -0,0 +1,19 @@
+let arr = [1, "hi", 2, 7,"hello"];
+let num = [];
+let str = [];
+
+for (let i = 0; i < arr.length; i++){
+    if (arr[i] === Number(arr[i])){
+        num.push(arr[i]);
+    }
+    else  if(arr[i] === String(arr[i])){
+        str.push(arr[i])
+    }
+    else {
+        console.log("any types");
+    }
+}
+console.log(`"Numbers: ${num.length}, Strings: ${str.length}"`);
+
+
+
